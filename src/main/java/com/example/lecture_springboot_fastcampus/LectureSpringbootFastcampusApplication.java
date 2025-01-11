7373847edb080126a6ae8151f49073905fd5ddec
ayz1070 <ayz1070@gmail.com>
2025-01-12 08:54:56 +0900

    [init] 프로젝트 초기화
    - 프로젝트 초기화

---
+++ src/main/java/com/example/lecture_springboot_fastcampus/LectureSpringbootFastcampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecture_springboot_fastcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSpringbootFastcampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureSpringbootFastcampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lecture_springboot_fastcampus/LectureSpringbootFastcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture_springboot_fastcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSpringbootFastcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
